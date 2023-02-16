cf53464cd39c7f402c2aef51ad8be664798314b8
pickleali <iafh.gm@gmail.com>
2023-02-16 17:17:30 +0300

    created class 'Car' and 'Article' file

--- src/main/kotlin/Article.kt
+++ src/main/kotlin/Article.kt
@@ -6,7 +6,20 @@
  * 4. Practice 'vararg': To pass a variable number of arguments to a function [instead of a,b,c,d..etc. One argument is enough]
  **/
 
+// To inherit a superclass, write the name of superclass after the parentheses of subclass.
+// Also, it is required to identify the superclass as an 'open class'.
+// Example: class Article(a, b, c) : Website()
+//          open class website()
+// Abstract class: instead of writing 'open' before class, write 'abstract' keyword in order to make it an abstract.
+//                 The purpose of abstract class is to initialize a function for all classes and each class has its own definition.
+//                 Example: area() definition for rectangle is different from triangle.
+// As for visibility modifiers (public,private..etc),
+// check kotlin.org docs for clear demonstration: https://kotlinlang.org/docs/visibility-modifiers.html#class-members
+
 class Article(
+    // to make constructor of a class private,
+    // write: class Article(...) private constructor {...}
+
     private val website: String,
     private val topic: String,
     private val tag: String? = null
